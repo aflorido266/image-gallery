bc06deaad4795731e21c910f24c11008404e6e38
Ada <aflorido266@gmail.com>
2026-05-13 15:52:06 +0200

    architectural correction

--- src/app/image/image.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-image',
-  imports: [],
-  templateUrl: './image.html',
-  styleUrl: './image.css',
-})
-export class Image {}
