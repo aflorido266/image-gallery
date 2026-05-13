02fc9b86ce5443f467452c22692d09b657a51aac
Ada <aflorido266@gmail.com>
2026-05-13 16:37:16 +0200

    add interface logica

--- src/app/features/gallery/gallery.ts
+++ src/app/features/gallery/gallery.ts
@@ -1,9 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { IImage } from '../../interfaces/image.interface';
 
 @Component({
   selector: 'app-gallery',
+  standalone: true,
   imports: [],
   templateUrl: './gallery.html',
   styleUrl: './gallery.css',
 })
-export class Gallery {}
+export class Gallery {
+
+  images = signal<IImage[]>([
+    {
+      id: 1,
+      url: 'https://pravatar.cc/150?img=1',
+      alt: 'Avatar 1'
+    },
+    {
+      id: 2,
+      url: 'https://pravatar.cc/150?img=2',
+      alt: 'Avatar 2'
+    },
+    {
+      id: 3,
+      url: 'https://pravatar.cc/150?img=3',
+      alt: 'Avatar 3'
+    },
+    {
+      id: 4,
+      url: 'https://pravatar.cc/150?img=4',
+      alt: 'Avatar 4'
+    },
+    {
+      id: 5,
+      url: 'https://pravatar.cc/150?img=5',
+      alt: 'Avatar 5'
+    },
+    {
+      id: 6,
+      url: 'https://pravatar.cc/150?img=6',
+      alt: 'Avatar 6'
+    },
+    {
+      id: 7,
+      url: 'https://pravatar.cc/150?img=7',
+      alt: 'Avatar 7'
+    },
+    {
+      id: 8,
+      url: 'https://pravatar.cc/150?img=8',
+      alt: 'Avatar 8'
+    }
+  ]);
+
+}
